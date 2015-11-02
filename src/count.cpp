7642edf07aab4e6eac292ad09d3a3beac62eccf2
vineethakagitha <vineetha.muni@gmail.com>
2015-11-02 10:11:48 -0800

    Submit Commit

--- src/count.cpp
+++ src/count.cpp
@@ -13,8 +13,10 @@ NOTES:
 */
 int count(int num1, int num2) {
 	int i,count=0;
-	if (num1 <= 0 || num2 < 0 || num1>num2)
+	if (num1 <= 0 || num2 < 0 )
 		return -1;
+	if (num2 == 0)
+		return 0;
 	else{
 		for (i = num1; i <= num2; i++)
 		{
